5b1add60530aa922eb1bb8fca8a6d9051cf34539
Alex. A. Manetas <p3200282@aueb.gr>
2024-11-29 00:00:20 +0200

    Fixed camera switch

--- scripts/levels/level_01.gd
+++ scripts/levels/level_01.gd
@@ -5,19 +5,18 @@ extends Node3D
 @onready var rogue: Rogue = $Rogue
 @onready var knight: Knight = $Knight
 
+var temp_state
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	knight.active = true
 	rogue.active = false
 
-
 func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("player_switch"):
-		var temp_state = !$Knight.active
-		$Rogue.active = $Knight.active
-		$Knight.active = temp_state
-	
 	if event.is_action_pressed("player_switch"):
+		temp_state = !knight.active
+		rogue.active = knight.active
+		knight.active = temp_state
 		if camera1.is_current():
 			camera1.clear_current(true)
 		elif camera2.is_current():
