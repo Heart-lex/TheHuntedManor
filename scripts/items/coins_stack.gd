b4f4bbd6be7d7d2961d58dc887470aab31fa142d
Christy <xrysafhxristiana@gmail.com>
2024-12-09 18:33:46 +0200

    fixed lever, added shop and coin collector

--- scripts/items/coins_stack.gd
+++ scripts/items/coins_stack.gd
@@ -1,9 +1,11 @@
 extends Area3D
 
 signal coin_collected
+@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
+
 
 func _on_body_entered(body: Node3D)-> void:
-
 	if body.is_in_group("player"):
+		audio_player.play()
 		emit_signal("coin_collected")
 		queue_free()
