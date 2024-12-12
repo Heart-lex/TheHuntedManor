d8267a19677a932fdd76507dba08188b911b92b2
Alex. A. Manetas <p3200282@aueb.gr>
2024-12-12 23:40:22 +0200

    Foundation now has smoother, more consistent movement

--- scripts/items/movable_foundation.gd
+++ scripts/items/movable_foundation.gd
@@ -2,24 +2,30 @@ extends CharacterBody3D
 
 @onready var foundation: Node3D = $Model
 
+const SPEED : float = 0.1
+
 # None, PositiveX, NegativeX, PositiveZ, NegativeZ
 enum Zones { NONE, POS_X, NEG_X, POS_Z, NEG_Z }
 
 # Set initial zone to "None"
 var current_zone : Zones = Zones.NONE
 
-func _process(delta: float) -> void:
-	
-	move_and_slide()
-	
+func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("interact") and current_zone != Zones.NONE:
 		print("Currently at: " + Zones.keys()[current_zone]) # debug
 		move(current_zone)
 
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+	
+	if velocity.x:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if velocity.z:
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Moves based on the zone in which the Knight is at.
 #
-# Moves towards the according direction of the X/Y axis,
+# Moves towards the according direction of the X/Z axis,
 # "away" from the zone that the Knight is touching.
 # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 func move(zone: Zones) -> void:
@@ -27,13 +33,13 @@ func move(zone: Zones) -> void:
 		Zones.NONE:
 			pass
 		Zones.POS_X:
-			velocity.x += 1
+			velocity.x = 3
 		Zones.NEG_X:
-			velocity.x -= 1
+			velocity.x = -3
 		Zones.POS_Z:
-			velocity.z += 1
+			velocity.z = 3
 		Zones.NEG_Z:
-			velocity.z -= 1
+			velocity.z = -3
 		
 		# I used the following when it was a simple Node3D:
 		#Zones.POS_X:
