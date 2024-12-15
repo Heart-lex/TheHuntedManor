66181089e80daa47de919c1509e32140353fa1ea
Christy <xrysafhxristiana@gmail.com>
2024-12-15 16:59:59 +0200

    Added script to Knight's hurtbox

--- scripts/shared/hurtbox.gd
+++ scripts/shared/hurtbox.gd
@@ -1,3 +1,13 @@
 extends Area3D
 
 @export var health_component: HealthComponent
+
+func _ready() -> void:
+	self.hide()
+
+func start_attack() -> void:
+	self.show()
+
+func stop_attack() -> void:
+	self.hide()
+	
