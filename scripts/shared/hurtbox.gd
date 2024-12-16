b5929705b1167ad4c8c7bbe7324d8e677e625e2b
Alex. A. Manetas <p3200282@aueb.gr>
2024-12-16 22:30:09 +0200

    Added comment

--- scripts/shared/hurtbox.gd
+++ scripts/shared/hurtbox.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+# Exported variable to allow for enemies that always
+# damage you on contact, defaults to false
 @export var always_enabled = self.monitoring
 
 func _ready() -> void:
@@ -7,10 +9,8 @@ func _ready() -> void:
 
 func start_attack() -> void:
 	self.monitoring = true
-	print("ATTACK!")
 
 func stop_attack() -> void:
 	self.monitoring = false
-	print("Stopped attacking...")
 
 	
