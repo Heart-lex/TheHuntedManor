c9c12bba965d15dd5e985b7a7cbc18a4c303bf7b
Christy <xrysafhxristiana@gmail.com>
2024-12-16 09:33:28 +0200

    added input function for the game manager to handle potions (needs to be updated)

--- scripts/globals/game_manager.gd
+++ scripts/globals/game_manager.gd
@@ -10,17 +10,32 @@ func set_heroes(knight : Knight, rogue: Rogue) -> void:
 	hero_knight = knight
 	hero_rogue = rogue
 	
-	
 func set_knight_as_active() -> void:
 	hero_knight.active = true
 	hero_rogue.active = false
 	
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("use_blue_potion"):
+		if CoinCollector.blue_potion_count > 0: 
+			CoinCollector.blue_potion_count -=1
+	if Input.is_action_just_pressed("use_green_potion"):
+		if CoinCollector.green_potion_count > 0: 
+			CoinCollector.green_potion_count -=1
+	if Input.is_action_just_pressed("use_red_potion"):
+		if CoinCollector.red_potion_count > 0: 
+			CoinCollector.red_potion_count -=1
+			if hero_knight.active:
+				hero_knight.health_component += 10
+			else:
+				hero_rogue.health_component += 10
+	if Input.is_action_just_pressed("use_purple_potion"):
+		if CoinCollector.purple_potion_count > 0: 
+			CoinCollector.purple_potion_count -=1
 	
 func set_rogue_as_active() -> void:
 	hero_rogue.active = true
 	hero_knight.active = false
-	
-	
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
