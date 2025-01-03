80e69a5cd49968fe73c25527c9097f00803b2f14
Christy <xrysafhxristiana@gmail.com>
2025-01-03 18:15:09 +0200

    automatic change to the next scene

--- scripts/items/exit_lever.gd
+++ scripts/items/exit_lever.gd
@@ -11,6 +11,9 @@ extends CharacterBody3D
 var active: bool = false
 var is_triggered: bool = false
 
+var level_number: int = 1
+var base_scene_path: String = "res://scenes/levels/"
+
 func _ready() -> void:
 	interaction_prompt.visible = false
 	animation_tree.set("parameters/LeverToggle/transition_request", "Idle")
@@ -24,7 +27,11 @@ func _process(delta: float) -> void:
 		SceneTransitionAnimation.animation_player.play("fade_in")
 		await get_tree().create_timer(0.5).timeout
 		AudioManager.play_sound(AudioManager.LEVEL_SUCCESS, 0.25, 1)
-		get_tree().change_scene_to_file("res://scenes/levels/level02.tscn")
+		level_number += 1
+		if level_number != 6:
+			var next_scene_path = base_scene_path + str(level_number).pad_zeros(2) + ".tscn"
+			get_tree().change_scene_to_file(next_scene_path)
+		
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("rogue"):
